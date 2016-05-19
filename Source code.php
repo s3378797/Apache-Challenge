c8b3f06eb28b6226f4676764dc0a247ec50737e1
s3378797 <s3378797@student.rmit.edu.au>
2016-05-19 22:29:51 +1000

    Create Source code.php

---
+++ Source code.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<a href="index.php" target="_self">Back to index</a> 
+<br>
+<h2>Source code </h2>
+<br>
+<?php highlight_file('index.php'); ?>
+<br><br>
+	
+<!--
+<a href="JavaScript:window.close()">Close window</a>
+-->
+
+
+</body>
+</html>
